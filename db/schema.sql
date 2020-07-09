2fb138a0a6bb6acbce509f964080ddda08a8479d
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2020-07-09 18:55:26 -0400

    initial files added

---
+++ db/schema.sql
@@ -0,0 +1,32 @@
+/*
+
+To run this file, we do the following in our Terminal:
+
+1. Go to the directory of this sql file.
+
+2. Get into our mysql console.
+
+3. Run "source schema.sql"
+
+*/
+
+-- Drops the task_saver_db if it already exists --
+DROP DATABASE IF EXISTS burgers_db;
+
+-- Create the database task_saver_db and specified it for use.
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+-- Create the table tasks.
+CREATE TABLE burgers (
+  id int NOT NULL AUTO_INCREMENT,
+  burger_name varchar(255) NOT NULL,
+  devoured boolean,
+  PRIMARY KEY (id)
+);
+
+-- Insert a set of burgers.
+-- INSERT INTO burgers (task) VALUES ('Pick up milk.');
+-- INSERT INTO burgers (task) VALUES ('Mow the lawn.');
+-- INSERT INTO burgers (task) VALUES ('Call Shannon back.');
